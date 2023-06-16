d084ce6c5c2f39abd48e9d414598ed47d55ffb85
Mikhail Knyazhev <markus621@yandex.ru>
2023-06-16 05:53:00 +0300

    update license

--- migrations.go
+++ migrations.go
@@ -1,3 +1,9 @@
+/* 
+ *  LGPL-3.0 license
+ *  Copyright (c) 2023 Mikhail Knyzhev <markus621@yandex.ru>
+ *  See the full text of the license in the LICENSE file in the root directory.
+ */
+
 package hermesaddons
 
 type MigrationsGetter interface {
