3b43d6a420f5d90aa9e5366ded773adbc20a4c90
Mikhail Knyazhev <markus621@gmail.com>
2023-06-24 01:22:31 +0300

    ...

---
+++ dependency/database.go
@@ -0,0 +1,36 @@
+package dependency
+
+import "context"
+
+type (
+	ORM interface {
+		ExecContext(name string, ctx context.Context, call func(q Executor)) error
+		QueryContext(name string, ctx context.Context, call func(q Querier)) error
+		TransactionContext(name string, ctx context.Context, call func(v Tx)) error
+	}
+
+	Result interface {
+		RowsAffected() int64
+		LastInsertId() int64
+	}
+
+	Executor interface {
+		SQL(query string, args ...interface{})
+		Params(args ...interface{})
+		Bind(call func(result Result) error)
+	}
+
+	Scanner interface {
+		Scan(args ...interface{}) error
+	}
+
+	Querier interface {
+		SQL(query string, args ...interface{})
+		Bind(call func(bind Scanner) error)
+	}
+
+	Tx interface {
+		Exec(vv ...func(e Executor))
+		Query(vv ...func(q Querier))
+	}
+)
